7950f62869c5c090f43fd3010064ab03c9fec711
Vasu Singh <vasusingh1305@gmail.com>
2025-05-01 02:29:36 +0530

    changes made

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,55 +1,91 @@
+"use client"
 import Button from "@/components/Button";
 import designExample1Image from "@/assets/images/design-example-1.png"
 import designExample2Image from "@/assets/images/design-example-2.png"
 
-import DesignImage from "@/components/DesignImage";
+
 import Pointer from "@/components/Pointer";
+import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
+import Image from "next/image";
 
 
 
 export default function Hero() {
+    const [leftDesignScope, leftDesignAnimate] = useAnimate();
+    const [leftPointerScope, leftPointerAnimate] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate]  = useAnimate()
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+    useEffect(()=>{
+        leftDesignAnimate([
+            [leftDesignScope.current, {opacity: 1}, {duration: 0.5}],
+            [leftDesignScope.current, {y:0, x:0}, {duration: 0.5}],
+
+        ])
+        leftPointerAnimate([
+            [leftPointerScope.current, {opacity:1}, {duration:0.5}],
+            [leftPointerScope.current, {y:0, x:-100}, {duration: 0.5}],
+            [leftPointerScope.current, {x:0, y:[0,16,0]}, {duration: 0.5}]
+
+        ])
+        rightDesignAnimate([
+            [rightDesignScope.current, {opacity:1}, {duration:0.5}],
+            [rightDesignScope.current, {y:0, x:0}, {duration:0.5}]
+        ])
+        rightPointerAnimate([
+            [rightPointerScope.current , {opacity:1}, {duration:0.5}],
+            [rightPointerScope.current, {y:0, x:200}, {duration: 0.5}]
+
+        ])
+        
+    }, []);
+
     return <section className="py-24 overflow-x-clip">
         <div className="container relative">
 
             {/* ---------- Made myself a reusable component -------- */}
 
-            {/* <div className="absolute -left-32 top-16 hidden lg:block">
+            <motion.div ref={leftDesignScope} initial={{opacity:0, y:100, x:-100}} className="absolute -left-32 top-16 hidden lg:block">
                 <Image src={designExample1Image} alt="Image1" />
 
-            </div>
-            <div className="absolute -right-64 -top-16">
-                <Image src={designExample2Image} alt="Image2" />
-            </div> */}
-            <DesignImage src={designExample1Image} type="left" alt="hero" />
-            <DesignImage src={designExample2Image} type="right" alt="right hero" />
-            <div className="absolute top-96 left-56 hidden lg:block">
+            </motion.div>
+            {/* <motion.div ref={leftDesignScope} initial={{opacity:0 , y:100, x:-100}} >
+                <DesignImage src={designExample1Image} type="left" alt="hero" />
+
+                </motion.div> */}
+
+            <motion.div  ref={leftPointerScope} initial={{opacity: 0, y: 100, x:-200}} className="absolute top-96 left-56 hidden lg:block">
                 <Pointer name="vasu" color="red" />
-               
-            </div>
-            <div className="absolute -top-4 right-80 hidden lg:block">
+
+            </motion.div>
+            {/* <DesignImage src={designExample2Image} type="right" alt="right hero" /> */}
+            <motion.div  ref={rightDesignScope} initial={{opacity:0, y:100, x:100 }} className="absolute -right-64 -top-16">
+                <Image src={designExample2Image} alt="Image2" />
+            </motion.div>
+            <motion.div ref={rightPointerScope} initial={{opacity:0, y:100, x:300}} className="absolute -top-4 right-80 hidden lg:block">
                 <Pointer name="Kritagya"  />
-               
-            </div>
-            
+
+            </motion.div>
+
             <div className="flex justify-center">
                 <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950     font-semibold ">✨ $7.5M seed round raised</div>
 
             </div>
-            
+
             <h1 className="mt-6 text-6xl font-medium text-center md:text-7xl lg:text-8xl">Impactful design, created effortlessly</h1>
             <p className="mt-8 text-center text-xl text-white/50 max-w-2xl mx-auto">Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intuitive interface that keeps you in your creative flow.</p>
-            
+
 
             <form action="" className="flex border border-white/15  rounded-full  p-2 mt-8 w-full max-w-lg mx-auto ">
                 <input type="email" name="" id="" placeholder="Enter your email" className="bg-transparent px-4 min-w-0 flex-1" />
-                
+
                 <Button variant="primary" type="submit" className="whitespace-nowrap " >Sign Up</Button>
             </form>
 
-          
-            
+
+
 
         </div>
-            
+
     </section>
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -17,70 +17,78 @@ const navLinks = [
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
-    return <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
-        <div className="container max-w-5xl">
-            <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur ">
-                <div className="grid grid-cols-2 lg:grid-cols-3  p-2 items-center px-4 md:pr-2 ">
-                    <div>
-                        <Image src={logoImage} alt="logo"  className="h-9 md:h-auto w-auto" />
-                        
+    return (
+    <>
+   
+        <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
+            <div className="container max-w-5xl">
+                <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur ">
+                    <div className="grid grid-cols-2 lg:grid-cols-3  p-2 items-center px-4 md:pr-2 ">
+                        <div>
+                            <Image src={logoImage} alt="logo"  className="h-9 md:h-auto w-auto" />
+                            
 
-                    </div>
-                    <div className="hidden lg:flex justify-center">
-                        <nav className="flex gap-6 font-medium">
-                            {navLinks.map(link =>(
-                                <a href={link.href} key={link.label}>{link.label}</a>
+                        </div>
+                        <div className="hidden lg:flex justify-center">
+                            <nav className="flex gap-6 font-medium">
+                                {navLinks.map(link =>(
+                                    <a href={link.href} key={link.label}>{link.label}</a>
 
-                            ))}
-                        </nav>
-                    </div>
-                    <div className="flex justify-end gap-4 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-menu cursor-pointer md:hidden"onClick={()=>setIsOpen(!isOpen)} >
-                        <line x1="3" y1="6" x2="21" y2="6" className={twMerge("origin-left transition-transform duration-300  ", isOpen && "rotate-45 -translate-y-1")} ></line>
-                  
-                                <line x1="3" y1="12" x2="21" y2="12" className={twMerge( "transition", isOpen && 'opacity-0')}></line>
-                        <line x1="3" y1="18" x2="21" y2="18" className={twMerge("origin-left transition duration-300 ", isOpen && " -rotate-45 translate-y-1")} ></line>
+                                ))}
+                            </nav>
+                        </div>
+                        <div className="flex justify-end gap-4 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-menu cursor-pointer md:hidden" onClick={()=>setIsOpen(!isOpen)} >
+                            <line x1="3" y1="6" x2="21" y2="6" className={twMerge("origin-left transition-transform duration-300  ", isOpen && "rotate-45 -translate-y-1")} ></line>
                     
-            
-                        </svg>
-                        <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
-                        <Button variant="primary" className="hidden md:inline-flex items-center">Sign Up</Button>
+                                    <line x1="3" y1="12" x2="21" y2="12" className={twMerge( "transition", isOpen && 'opacity-0')}></line>
+                            <line x1="3" y1="18" x2="21" y2="18" className={twMerge("origin-left transition duration-300 ", isOpen && " -rotate-45 translate-y-1")} ></line>
                         
-                     
-                </div>
-
-                </div>
-                <AnimatePresence>
-
                 
-                {isOpen && (
-                            <motion.div initial={{height:0}} animate={{height:"auto"}} exit={{height:0}} className="overflow-hidden">
-                                <div className="flex flex-col gap-2 items-center py-4 md:hidden">
-                                         {navLinks.map((link)=>(
-
-                                                <a href={link.href} key={link.label} className="py-1 px-3 rounded-2xl  hover:bg-neutral-700" >{link.label}</a>
-
-                                            ))}
-                                            <div className="flex gap-4 ">
-                                                <Button variant="primary" className="hover:bg-lime-600 ">Login</Button>
-                                                <Button variant="secondary" className="hover:bg-neutral-900">Sign Up</Button>
-
-                                            </div>
-
-                                </div>
+                            </svg>
+                            <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
+                            <Button variant="primary" className="hidden md:inline-flex items-center">Sign Up</Button>
                             
+                        
+                    </div>
+
+                    </div>
+                    <AnimatePresence>
+
+                    
+                    {isOpen && (
+                                <motion.div initial={{height:0}} animate={{height:"auto"}} exit={{height:0}} className="overflow-hidden">
+                                    <div className="flex flex-col gap-2 items-center py-4 md:hidden">
+                                            {navLinks.map((link)=>(
+
+                                                    <a href={link.href} key={link.label} className="py-1 px-3 rounded-2xl  hover:bg-neutral-700" >{link.label}</a>
+
+                                                ))}
+                                                <div className="flex gap-4 ">
+                                                    <Button variant="primary" className="hover:bg-lime-600 ">Login</Button>
+                                                    <Button variant="secondary" className="hover:bg-neutral-900">Sign Up</Button>
+
+                                                </div>
+
+                                    </div>
+                                
+                                
+
+                                </motion.div>
                             
-
-                            </motion.div>
-                          
-                ) }
-                </AnimatePresence>
-              
-
+                    ) }
+                    </AnimatePresence>
                 
-            </div>
+
+                    
+                </div>
+                
             
-          
+            </div>
+        </section>
+        <div className="pb-[52px] md:pb-[64px] lg:pb-[129px]">
+
         </div>
-    </section>
+    </>
+    )
 }
